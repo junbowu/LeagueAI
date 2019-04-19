0e853d3f182381f74d06f902d289343570ec3f0c
Oleffa <oliver.struckmeier@onlinehome.de>
2019-04-19 19:02:51 +0300

    Minimal example working

--- LeagueAI_minimal_example.py
+++ LeagueAI_minimal_example.py
@@ -1,27 +1,33 @@
-from LeagueAI import input_output, LeagueAIFramework
+from LeagueAI_helper import input_output, LeagueAIFramework, detection
 import time
 import cv2
 
 ####### Params ######
 # Show the AI view or not:
 show_window = True
+# Output window size
+output_size = int(3440/3), int(1440/3)
 # To record the desktop use:
-IO = input_output(input_mode='desktop', SCREEN_WIDTH=1080, SCREEN_HEIGHT=1920)
+#IO = input_output(input_mode='desktop', SCREEN_WIDTH=1920, SCREEN_HEIGHT=1080)
 # If you want to use the webcam as input use:
 #IO = input_output(input_mode='webcam')
 # If you want to use a videofile as input:
-#IO = input_output(input_mode='videofile', video_filename='video.mp4')
+IO = input_output(input_mode='videofile', video_filename='videos/video.mp4')
 ####################
 
-LeagueAI = LeagueAIFramework()
-
+LeagueAI = LeagueAIFramework(config_file="cfg/LeagueAI.cfg", weights="weights/04_16_LeagueAI.weights", names_file="cfg/LeagueAI.names", classes_number = 5, resolution=960, threshold = 0.55, cuda=True, draw_boxes=True)
 while True:
     start_time = time.time()
     # Get the current frame from either a video, a desktop region or webcam (for whatever reason)
     frame = IO.get_pixels()
-    # Get the dictionary of detected objects and their positions
-    objects = LeagueAI.get_objects()
-    print(objects)
+    # Get the list of detected objects and their positions
+    objects = LeagueAI.get_objects(frame)
+    for o in objects:
+        o.toString()
+        # Example to validate if the returned objects are correct:
+        # Draw rectangles arount the objects and a center point, you can use the draw_boxes=False to turn the frameworks boxes off
+        #cv2.circle(frame, (o.x, o.y), 5, (255,0,0), -1)
+        #cv2.rectangle(frame, (o.x_min, o.y_min), (o.x_max, o.y_max), (255, 0, 0), 4)
 
     """
     Here you can do stuff with the detected objects and the other data from above.
@@ -31,11 +37,11 @@ while True:
     """
 
     # Write fps
-    IO.show_output_window(show_window)
     cycle_time = time.time()-start_time
-    cv2.putText(frame, "FPS: {}".format(str(round(1/cycle_time,0))), (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2)
-    # Show the AI view
+    cv2.putText(frame, "FPS: {}".format(str(round(1/cycle_time,2))), (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2)
+    # Show the AI view, rescale to output size
     if show_window:
+        frame = cv2.resize(frame, output_size)
         cv2.imshow('LeagueAI', frame)
         if (cv2.waitKey(25) & 0xFF == ord('q')):
             cv2.destroyAllWindows()
